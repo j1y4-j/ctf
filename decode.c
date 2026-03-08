133eb4c2e209318e4f144448eb4383375dd00870
Jiya <jiyajayaprakash.241cs230@nitk.edu.in>
2026-03-08 15:48:30 +0530

    Updated

--- decode.c
+++ decode.c
@@ -1,17 +1,57 @@
 #include <stdio.h>
+#include <string.h>
+
+int verify(char *input)
+{
+
+    int seed = 4;
+
+    int table[] = {112, 118, 101, 106, 119, 103, 97, 106, 96, 109, 106, 99};
+
+    for (int i = 0; i < 12; i++)
+    {
+        if ((input[i] ^ seed) != table[i])
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+void reveal()
+{
+
+    int key = 3;
+
+    int hidden[] = {109, 102, 112, 96, 98, 101, 102};
+
+    printf("Location unlocked: ");
+
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%c", hidden[i] ^ key);
+    }
+
+    printf("\n");
+}
 
 int main()
 {
 
-    int enc[] = {113, 104, 118, 102, 100, 105, 104};
-    int key = 3;
+    char buffer[50];
 
-    for (int i = 0; i < 7; i++)
+    printf("Enter access code: ");
+    scanf("%s", buffer);
+
+    if (verify(buffer))
     {
-        printf("%c", enc[i] - key);
+        reveal();
     }
-
-    printf("\n");
+    else
+    {
+        printf("Access denied\n");
+    }
 
     return 0;
 }
